733eff21a48862f8c73d6cea7190f10b94dc3868
Simone Poggesi <117054508+spoggesi@users.noreply.github.com>
2024-08-28 12:17:21 +1200

    Update rt-task_main.js

--- rt-task_main.js
+++ rt-task_main.js
@@ -75,7 +75,7 @@ var test = {
     stimulus: jsPsych.timelineVariable('stimulus'),
     choices: ['f', 'j'],
     data: jsPsych.timelineVariable('data'),
-    prompt: 'NO YES', rows: 5,
+    prompt: 'NO YES',
     on_finish: function (data) {
         data.correct = data.key_press == jsPsych.pluginAPI.convertKeyCharacterToKeyCode(data.correct_response);
     },
